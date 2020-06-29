2b6ff66c5b6fc3f8ad9c98a96f6c9f0c24423eb3
Victor Park <victor.shpark@gmail.com>
2020-06-29 00:35:38 -0300

    navbar created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// For hamburger menu dropdown action
+const changeBurger = () => {
+  document.querySelector(".hamburger").classList.toggle("switch-patties");
+  document.querySelector("header").classList.toggle("reveal");
+}
