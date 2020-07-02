6eb27a91501a5c4c6eae076dd1f8fde70b6316bf
Victor Park <victor.shpark@gmail.com>
2020-07-02 17:52:21 -0300

    views - edit and new stylized

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -9,7 +9,11 @@ class PicsController < ApplicationController
   end
 
   def new
-    @pic = current_user.pics.build
+    if user_signed_in?
+      @pic = current_user.pics.build
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
