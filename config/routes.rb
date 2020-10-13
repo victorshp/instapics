d991ba220ad17460ec65a5032cd407b4a829cef0
Victor Park <victor.shpark@gmail.com>
2020-10-13 07:53:37 -0300

    root to pages home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "pics#index"
+  root to: "pages#home"
 
   resources :pics, only: [:index, :show, :new, :create, :destroy] do
     member do
